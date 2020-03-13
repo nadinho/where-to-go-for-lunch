792e47416475d44efd811466644bb261b7d60008
nadinho <rockt2935@gmail.com>
2020-03-13 15:20:48 +0100

    create links between pages

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import './thanks.css';
+import './Thanks.css';
+import { Link } from 'react-router-dom';
 
-export default function() {
-  return <></>;
+export default function Start() {
+  return (
+    <>
+      <p>Danke für die Blumen...</p>
+      <p>
+        <Link to="/">Zurück zur Startseite</Link>
+      </p>
+    </>
+  );
 }
