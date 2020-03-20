328e587025bb279fcbb4572a3ee86a5d33caf4f4
nadinho <rockt2935@gmail.com>
2020-03-20 16:04:19 +0100

    add box shadow and adjust color

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,15 +6,17 @@ const Button = styled.button`
 
   height: 50px;
   width: 40%;
+  border: none;
   border-top-left-radius: 40px;
   border-bottom-left-radius: 40px;
   position: fixed;
   bottom: 20px;
+  box-shadow: 2px 2px 5px ${props => props.theme.colors.shadow};
   right: 0px;
   font-size: 1rem;
   cursor: pointer;
   font-family: Rubik;
   font-weight: 500;
-  color: ${props => props.theme.colors.textPrimary};
+  color: ${props => props.theme.colors.heading};
 `;
 export default Button;
--- src/themes/darkTheme.js
+++ src/themes/darkTheme.js
@@ -1,10 +1,10 @@
 const darkTheme = {
   colors: {
     primary: '#954CF2',
+    shadow: '#6d00cc',
     background: '#1a1a1a',
     card: '#2D1B36',
     textPrimary: '#fff',
-    textSecondary: 'fff',
     heading: '#f9f9f9'
   }
 };
